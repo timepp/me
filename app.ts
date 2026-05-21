92dcd22264195e9d694301d90486cba125988be0
Junhui Tong <junhuitong@microsoft.com>
2026-05-21 19:16:45 +0800

    update

--- app.ts
+++ app.ts
@@ -1,15 +1,14 @@
 import { parseArgs } from "jsr:@std/cli@0.224.7/parse-args"
 import { apiImpl } from './api-impl.ts'
-import * as denoUI from "jsr:@timepp/dui@0.1.11"
+import * as denoUI from "jsr:@timepp/dui@0.1.14"
 
 const args = parseArgs(Deno.args)
 const release = args.release || !import.meta.url.startsWith('file://')
 const memoryAssets = release? (await import('./release-assets.ts')).assets : {}
-
 await denoUI.startDenoUI({
     appName: 'My Environment',
     frontendRoot: 'frontend',
-    appMode: false,
+    appMode: args.appMode,
     apiImpl,
     release,
     memoryAssets
